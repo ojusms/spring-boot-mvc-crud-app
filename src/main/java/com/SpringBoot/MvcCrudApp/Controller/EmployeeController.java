345bd1c1b91bed70eb81aef6f657e601762a4838
Ojus M S <ojusms@gmail.com>
2026-06-19 21:12:57 -0700

    Add Controller package, EmployeeController class with a GET mapping to list employees

---
+++ src/main/java/com/SpringBoot/MvcCrudApp/Controller/EmployeeController.java
@@ -0,0 +1,38 @@
+package com.SpringBoot.MvcCrudApp.Controller;
+
+
+import com.SpringBoot.MvcCrudApp.Entity.Employee;
+import com.SpringBoot.MvcCrudApp.Service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    // define field for EmployeeService
+    EmployeeService employeeService;
+
+    // define constructor for dependency injection.
+    // @Autowired annotation is optional since there is only one constructor
+    public EmployeeController(EmployeeService theEmployeeService){
+        employeeService = theEmployeeService;
+    }
+
+    // add mapping for "/list"
+
+    @GetMapping("/list")
+    public String listEmployees(Model theModel){
+        // retrieve list of employees from database
+        List<Employee> employees = employeeService.findAll();
+
+        // add employees to Model with addAttribute()
+        theModel.addAttribute("employees", employees);
+
+        return "list-employees";
+    }
+}
