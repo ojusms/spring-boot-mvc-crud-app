3a7d37bdf9adf2bdd5ede7ef0de71154c5f92e94
Ojus M S <ojusms@gmail.com>
2026-06-19 19:28:43 -0700

    Initial commit - Spring Boot project from Spring Initializr

---
+++ src/main/java/com/SpringBoot/MvcCrudApp/MvcCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.MvcCrudApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcCrudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBoot/MvcCrudApp/MvcCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.MvcCrudApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
