f4c6ea0a71a057d61d3215fbb0e3a865584bf3af
Ojus M S <ojusms@gmail.com>
2026-06-21 19:40:38 -0700

    Add a custom method to EmployeeRepository extending JPA Repository to use Spring Data JPA to return a list of employees sorted by last name, update EmployeeService to call this method for findAll()

--- src/main/java/com/SpringBoot/MvcCrudApp/DAO/EmployeeRepository.java
+++ src/main/java/com/SpringBoot/MvcCrudApp/DAO/EmployeeRepository.java
@@ -3,7 +3,12 @@ package com.SpringBoot.MvcCrudApp.DAO;
 import com.SpringBoot.MvcCrudApp.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // provides some methods out of the box for DB interactions
+
+    // custom method for list of employees sorted by last name in asc
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
