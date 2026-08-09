b2c0b864082866fd23ccd398d6b5bdff185683aa
Ojus M S <ojusms@gmail.com>
2026-08-09 15:48:17 -0700

    Update LoggingAspect with a set up of Pointcut expressions

--- src/main/java/com/SpringBoot/MvcCrudApp/Aspect/LoggingAspect.java
+++ src/main/java/com/SpringBoot/MvcCrudApp/Aspect/LoggingAspect.java
@@ -1,6 +1,7 @@
 package com.SpringBoot.MvcCrudApp.Aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
@@ -13,4 +14,21 @@ public class LoggingAspect {
 
     // set up logger
     private Logger logger = Logger.getLogger(getClass().getName());
+
+    // set up pointcut expressions for Controller, DAO, and Service.
+
+    // The expression matches to any method (with 0 or more parameters of any type) of any return type
+    // inside any class within the respective package mentioned in the qualified name
+    @Pointcut("execution(* com.SpringBoot.MvcCrudApp.Controller.*.*(..))")
+    private void forController() {}
+
+    @Pointcut("execution(* com.SpringBoot.MvcCrudApp.DAO.*.*(..))")
+    private void forDAO() {}
+
+    @Pointcut("execution(* com.SpringBoot.MvcCrudApp.Service.*.*(..))")
+    private void forService() {}
+
+    // create a combined expression
+    @Pointcut("forController() || forDAO() || forService()")
+    private void forAppFlow() {}
 }
